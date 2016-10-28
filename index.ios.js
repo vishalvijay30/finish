ac168970528b9a4f5a4d29ce5c127b99d9e29ba8
vishalvijay30 <sinjaybrothers@gmail.com>
2016-10-27 23:26:06 -0400

    Basic navigation structure set up

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Navigator, AppRegistry, Text, View } from 'react-native';
+
+import TestScene from './scenes/TestScene'
 
 class Finish extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          Hello World!
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <Navigator
+        initialRoute = {{title: 'My Initial Scene', index:0}}
+        renderScene = {(route, navigator) =>
+
+          <TestScene
+            title = { route.title }
+
+            //function to call when a new scene is called
+            onForward = { () => {
+              const nextIndex = route.index + 1;
+              navigator.push({
+                title: 'Scene' + nextIndex,
+                index: nextIndex,
+              });
+            }}
+
+            //function to call to go back to the previous scene
+            onBack = { () => {
+              if (route.index > 0) {
+                navigator.pop();
+              }
+            }}
+          />
+        }
+      />
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Finish', () => Finish);
+AppRegistry.registerComponent('Finish', () => Finish)
