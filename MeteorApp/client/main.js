6f07e0724a4e540b4604d28f5cca108c030985b0
Aditya Shastri <ad.shastri1@gmail.com>
2016-12-21 15:57:38 -0500

    basic facebook functionality

--- MeteorApp/client/main.js
+++ MeteorApp/client/main.js
@@ -20,3 +20,21 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Template.login.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
---
+++ MeteorApp/server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: 'APP_ID_HERE',
+    secret: 'APP_SECRET_KEY_HERE'
+});
