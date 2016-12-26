fb75acb70ed5634985a862784c2e3d6f2e58a192
Aditya Shastri <ad.shastri1@gmail.com>
2016-12-26 14:59:09 -0500

    added add, update, remove methods and publish functionality

--- MeteorApp/server/main.js
+++ MeteorApp/server/main.js
@@ -4,3 +4,29 @@ import '/imports/api/items';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+	'addHabit': function(habit){
+		//expect habit to have {habitId: integer, title: String, streak: 0}
+		var currentUserId = this.userId();
+		Habits = new Mongo.Collection(currentUserId+"");
+		Habits.insert({userId: currentUserId, habitId: habit.habitId, title: habit.title, streak: habit.streak});
+	},
+	'removeHabit': function(habit) {
+		var currentUserId = this.userId();
+		Habits = new Mongo.Collection(currentUserId+"");
+		Habits.remove({habitId: habit.habitId});
+	},
+	'updateStreak': function(habit) {
+		//expect habit to have {habitId, integer, title: String, streak: int}
+		var currentUserID = this.userId();
+		Habits = new Mongo.Collection(currentUserId+"");
+		Habits.update({habitId: habit.habitId}, {$inc: {streak: 1}});	
+	}
+});
+
+Meteor.publish('habits', function habitsPublication() {
+			var currentUserId = this.userId();
+			Habits = new Mongo.Collection(currentUserId+"");
+			return Habits.find({userId: this.userId()}).fetch();
+});
