11bd12a8d95e71226ed8dfe93c38d9bc95ae2169
Aditya Shastri <ad.shastri1@gmail.com>
2016-12-27 22:20:20 -0500

    finished server side methods

--- MeteorApp/imports/api/habits.js
+++ MeteorApp/imports/api/habits.js
@@ -1,28 +1,26 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 
+Habits = new Mongo.Collection('habits');
+
 Meteor.methods({
 	'addHabit': function(habit){
 		//expect habit to have {habitId: integer, title: String, streak: 0}
-		var currentUserId = this.userId();
+		var currentUserId = Meteor.userId();
 		Habits = new Mongo.Collection(currentUserId+"");
 		Habits.insert({userId: currentUserId, habitId: habit.habitId, title: habit.title, streak: habit.streak});
+        console.log(Habits.find().fetch());
 	},
 	'removeHabit': function(habit) {
-		var currentUserId = this.userId();
-		Habits = new Mongo.Collection(currentUserId+"");
 		Habits.remove({habitId: habit.habitId});
 	},
 	'updateStreak': function(habit) {
 		//expect habit to have {habitId, integer, title: String, streak: int}
-		var currentUserID = this.userId();
-		Habits = new Mongo.Collection(currentUserId+"");
 		Habits.update({habitId: habit.habitId}, {$inc: {streak: 1}});	
 	}
 });
 
 Meteor.publish('habits', function habitsPublication() {
-			var currentUserId = this.userId();
-			Habits = new Mongo.Collection(currentUserId+"");
+			var currentUserId = Meteor.userId();
 			return Habits.find({userId: this.userId()}).fetch();
 });
--- MeteorApp/server/main.js
+++ MeteorApp/server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import '/imports/api/items';
+import '/imports/api/habits';
 
 Meteor.startup(() => {
   // code to run on server at startup
